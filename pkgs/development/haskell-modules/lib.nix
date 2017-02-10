c2dc3e3816eef930e572ce5597a064a05d51da6f
Sukant Hajra <rrudbskr5g@snkmail.com>
2017-02-09 21:35:17 -0600

    BROKEN: type error with this approach
    $ nix-build -A haskellPackages.hscolour --show-trace .
    
    error: while evaluating the attribute ‘setupCompilerEnvironmentPhase’ of the
        derivation ‘hscolour-1.24.1’ at
        ./pkgs/development/haskell-modules/generic-builder.nix:166:3:
    
    while evaluating the attribute ‘nativeBuildInputs’ of the derivation
        ‘ghc-8.0.1’ at
        ./pkgs/development/compilers/ghc/8.0.1.nix:21:3:
    
    while evaluating the attribute ‘nativeBuildInputs’ of the derivation
        ‘ghc-7.10.3’ at
        ./pkgs/development/compilers/ghc/7.10.3.nix:21:3:
    
    while evaluating the attribute ‘setupCompilerEnvironmentPhase’ of the
        derivation ‘hscolour-1.24.1’ at
        ./pkgs/development/haskell-modules/generic-builder.nix:166:3:
    
    while evaluating ‘optionalString’ at
        ./lib/strings.nix:138:26, called from
        ./pkgs/development/haskell-modules/generic-builder.nix:196:7:
    
    value is a function while a set was expected, at
        ./pkgs/development/haskell-modules/default.nix:7:4

--- pkgs/development/haskell-modules/lib.nix
+++ pkgs/development/haskell-modules/lib.nix
@@ -2,8 +2,10 @@
 
 rec {
 
+  #overrideCabal = drv: f: drv.overrideAttrs f;
+
   overrideCabal = drv: f: (drv.override (args: args // {
-    mkDerivation = drv: args.mkDerivation (drv // f drv);
+    mkDerivation = drv: (args.mkDerivation drv).override f;
   })) // {
     overrideScope = scope: overrideCabal (drv.overrideScope scope) f;
   };
